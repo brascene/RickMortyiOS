4bdce9b88c7131e604c8dc0a80b8d045684536c8
Dino <bra.scene@gmail.com>
2023-02-05 19:47:15 +0100

    Create api request

--- RickMorty/ApiClient/RMRequest.swift
+++ RickMorty/ApiClient/RMRequest.swift
@@ -9,5 +9,46 @@ import Foundation
 
 /// Object that represents single APi call
 final class RMRequest {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    private let endpoint: RMEndpoint
+    private let pathComponents: Set<String>
+    private let queryParams: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach { string += "/\($0)" }
+        }
+        
+        if !queryParams.isEmpty {
+            string += "?"
+            let argString = queryParams.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argString
+        }
+        
+        return string
+    }
+    
+    public let httpMethod = "GET"
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    // MARK: Public init
+    public init(endpoint: RMEndpoint, pathComponents: Set<String> = [], queryParams: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParams = queryParams
+    }
 }
