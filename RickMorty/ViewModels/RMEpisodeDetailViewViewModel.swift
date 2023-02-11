b27dd875a80a8d7ffac947f1779c1940e4160d0e
Dino <bra.scene@gmail.com>
2023-02-11 19:31:18 +0100

    Episode details view model

---
+++ RickMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  RMEpisodeDetailViewViewModel.swift
+//  RickMorty
+//
+//  Created by Dino Pelic on 11. 2. 2023..
+//
+
+import Foundation
+
+final class RMEpisodeDetailViewViewModel {
+    private let endpointUrl: URL?
+    
+    init(endpointUrl: URL?) {
+        self.endpointUrl = endpointUrl
+        fetchEpisodeData()
+    }
+    
+    private func fetchEpisodeData() {
+        guard let url = endpointUrl, let request = RMRequest(url: url) else {
+            return
+        }
+        RMService.shared.execute(request, expecting: RMEpisode.self) { result in
+            switch result {
+            case .success(let success):
+                print(String(describing: success))
+            case .failure(let failure):
+                print(String(describing: failure))
+            }
+        }
+    }
+}
---
+++ RickMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickMorty
+//
+//  Created by Dino Pelic on 11. 2. 2023..
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
